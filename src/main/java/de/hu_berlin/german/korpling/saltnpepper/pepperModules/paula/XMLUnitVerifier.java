6cf639b68d75ec190b9d1f5a48e74cf41c88b0c5
eladrion <eladrion@cs.uni-potsdam.de>
2011-07-18 13:23:32 +0000

    Creating XMLUnit Verifier
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/PAULAModules/trunk@1569 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/XMLUnitVerifier.java
@@ -0,0 +1,11 @@
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula;
+
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperFW.PepperConverter;
+
+public class XMLUnitVerifier {
+
+	public static void verify(PepperConverter converter){
+		
+		
+	}
+}
