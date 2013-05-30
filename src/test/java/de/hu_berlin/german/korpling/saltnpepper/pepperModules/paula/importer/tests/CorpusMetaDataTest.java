b04e38ed1a3a4d97da4ea67feaac3f6979e25ec8
FlorianZipser <f.zipser@gmx.de>
2013-05-30 11:30:25 +0000

    added feature Feature #141 (import of corpus and subcorpus metadata)
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/PAULAModules/branches/MultiFeatAndMetaData@8070 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/importer/tests/CorpusMetaDataTest.java
@@ -0,0 +1,93 @@
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula.importer.tests;
+
+import java.io.File;
+
+import org.eclipse.emf.common.util.URI;
+
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.CorpusDefinition;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.FormatDefinition;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperModulesFactory;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.testSuite.moduleTests.PepperImporterTest;
+import de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula.PAULAImporter;
+import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusGraph;
+
+public class CorpusMetaDataTest extends PepperImporterTest
+{
+	URI resourceURI= URI.createFileURI(new File(".").getAbsolutePath());
+	URI temproraryURI= URI.createFileURI(System.getProperty("java.io.tmpdir"));
+	
+//	private PAULAImporter fixture=null;
+//
+//	public PAULAImporter getFixture() {
+//		return fixture;
+//	}
+//
+//	public void setFixture(PAULAImporter fixture) {
+//		this.fixture = fixture;
+//	}
+	
+	public void setUp()
+	{
+		super.setFixture(new PAULAImporter());
+		
+		super.getFixture().setSaltProject(SaltFactory.eINSTANCE.createSaltProject());
+		super.setResourcesURI(resourceURI);
+		super.setTemprorariesURI(temproraryURI);
+		
+		//setting temproraries and resources
+		this.getFixture().setTemproraries(temproraryURI);
+		this.getFixture().setResources(resourceURI);
+		
+		//set formats to support
+		FormatDefinition formatDef= PepperModulesFactory.eINSTANCE.createFormatDefinition();
+		formatDef.setFormatName("paula");
+		formatDef.setFormatVersion("1.0");
+		this.supportedFormatsCheck.add(formatDef);
+	}
+	
+	public static String getTestFolder()
+	{
+		return("./src/test/resources/");
+	}
+	
+	public void testCorpusMetaData1()
+	{
+		File rootCorpus= new File(getTestFolder()+"CorpusMetaData/"+"rootCorpus/");
+		
+		//start: creating and setting corpus definition
+			CorpusDefinition corpDef= PepperModulesFactory.eINSTANCE.createCorpusDefinition();
+			FormatDefinition formatDef= PepperModulesFactory.eINSTANCE.createFormatDefinition();
+			formatDef.setFormatName("xml");
+			formatDef.setFormatVersion("1.0");
+			corpDef.setFormatDefinition(formatDef);
+			corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
+			this.getFixture().setCorpusDefinition(corpDef);
+		//end: creating and setting corpus definition
+		
+		SCorpusGraph importedSCorpusGraph= SaltFactory.eINSTANCE.createSCorpusGraph();
+		this.getFixture().getSaltProject().getSCorpusGraphs().add(importedSCorpusGraph);
+		
+		//runs the PepperModule
+		this.start();
+		
+		this.getFixture().getSaltProject().saveSaltProject_DOT(URI.createFileURI("d:/Test/paulaCorpusMeta/dot/"));
+		
+		
+		assertNotNull(importedSCorpusGraph.getSCorpora());
+		assertEquals(2,importedSCorpusGraph.getSCorpora().size());
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(0));
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations());
+		assertEquals(2,importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations().size());
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author"));
+		assertEquals("John Doe",importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author").getSValue());
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang"));
+		assertEquals("eng",importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang").getSValue());
+		
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(1));
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotations());
+		assertNotNull(importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotation("date"));
+		assertEquals("today",importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotation("date").getSValue());
+	}
+	
+}
