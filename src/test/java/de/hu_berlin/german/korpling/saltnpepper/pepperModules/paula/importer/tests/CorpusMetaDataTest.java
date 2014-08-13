79f7d62a346fe86506125bcf9f16a85149a54ad4
Florian Zipser <f.zipser@gmx.de>
2014-08-13 14:54:54 +0200

    set dependency to Pepper 2.0.1-SNAPSHOT

--- src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/importer/tests/CorpusMetaDataTest.java
+++ src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/importer/tests/CorpusMetaDataTest.java
@@ -30,57 +30,53 @@ import de.hu_berlin.german.korpling.saltnpepper.pepper.common.CorpusDesc;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.common.FormatDesc;
 import de.hu_berlin.german.korpling.saltnpepper.pepper.testFramework.PepperImporterTest;
 import de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula.PAULAImporter;
-import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusGraph;
 
-public class CorpusMetaDataTest extends PepperImporterTest
-{
-	
+public class CorpusMetaDataTest extends PepperImporterTest {
+
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		super.setFixture(new PAULAImporter());
-		
-		//set formats to support
-		FormatDesc formatDef= new FormatDesc();
+
+		// set formats to support
+		FormatDesc formatDef = new FormatDesc();
 		formatDef.setFormatName("paula");
 		formatDef.setFormatVersion("1.0");
 		this.supportedFormatsCheck.add(formatDef);
 	}
-	
+
 	@Test
-	public void testCorpusMetaData1()
-	{
-		File rootCorpus= new File(getTestResources()+"corpusMetaData/"+"rootCorpus/");
-		
-		//start: creating and setting corpus definition
-			CorpusDesc corpDef= new CorpusDesc();
-			FormatDesc formatDef= new FormatDesc();
-			formatDef.setFormatName("xml");
-			formatDef.setFormatVersion("1.0");
-			corpDef.setFormatDesc(formatDef);
-			corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
-			this.getFixture().setCorpusDesc(corpDef);
-		//end: creating and setting corpus definition
-		
-		//runs the PepperModule
+	public void testCorpusMetaData1() {
+		File rootCorpus = new File(getTestResources() + "corpusMetaData/" + "rootCorpus/");
+
+		// start: creating and setting corpus definition
+		CorpusDesc corpDef = new CorpusDesc();
+		FormatDesc formatDef = new FormatDesc();
+		formatDef.setFormatName("xml");
+		formatDef.setFormatVersion("1.0");
+		corpDef.setFormatDesc(formatDef);
+		corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
+		this.getFixture().setCorpusDesc(corpDef);
+		// end: creating and setting corpus definition
+
+		// runs the PepperModule
 		this.start();
-		
-		SCorpusGraph importedSCorpusGraph= getFixture().getSCorpusGraph();
+
+		SCorpusGraph importedSCorpusGraph = getFixture().getSCorpusGraph();
 		assertNotNull(importedSCorpusGraph.getSCorpora());
-		assertEquals(2,importedSCorpusGraph.getSCorpora().size());
+		assertEquals(2, importedSCorpusGraph.getSCorpora().size());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0));
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations());
-		assertEquals(2,importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations().size());
+		assertEquals(2, importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations().size());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author"));
-		assertEquals("John Doe",importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author").getSValue());
+		assertEquals("John Doe", importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author").getSValue());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang"));
-		assertEquals("eng",importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang").getSValue());
-		
+		assertEquals("eng", importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang").getSValue());
+
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(1));
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotations());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotation("date"));
-		assertEquals("today",importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotation("date").getSValue());
+		assertEquals("today", importedSCorpusGraph.getSCorpora().get(1).getSMetaAnnotation("date").getSValue());
 	}
-	
+
 }
--- src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/importer/tests/MultiFeatDataTest.java
+++ src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/paula/importer/tests/MultiFeatDataTest.java
@@ -34,109 +34,103 @@ import de.hu_berlin.german.korpling.saltnpepper.pepperModules.paula.PAULAImporte
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SCorpusGraph;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
 
-public class MultiFeatDataTest extends PepperImporterTest
-{
+public class MultiFeatDataTest extends PepperImporterTest {
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		super.setFixture(new PAULAImporter());
-			
-		//set formats to support
-		FormatDesc formatDef= new FormatDesc();
+
+		// set formats to support
+		FormatDesc formatDef = new FormatDesc();
 		formatDef.setFormatName("paula");
 		formatDef.setFormatVersion("1.0");
 		this.supportedFormatsCheck.add(formatDef);
 	}
-	
+
 	@Test
-	public void testMultiFeatData()
-	{
-		File rootCorpus= new File(getTestResources()+"multiFeatData/"+"myCorpus/");
-		assertTrue(rootCorpus.getAbsolutePath()+" does not exist",rootCorpus.exists());
-		assertTrue(rootCorpus.getAbsolutePath()+" is not a directory",rootCorpus.isDirectory());
-		
-		//start: creating and setting corpus definition
-			CorpusDesc corpDef= new CorpusDesc();
-			FormatDesc formatDef= new FormatDesc();
-			formatDef.setFormatName("xml");
-			formatDef.setFormatVersion("1.0");
-			corpDef.setFormatDesc(formatDef);
-			corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
-			this.getFixture().setCorpusDesc(corpDef);
-		//end: creating and setting corpus definition
-		
-		//runs the PepperModule
+	public void testMultiFeatData() {
+		File rootCorpus = new File(getTestResources() + "multiFeatData/" + "myCorpus/");
+		assertTrue(rootCorpus.getAbsolutePath() + " does not exist", rootCorpus.exists());
+		assertTrue(rootCorpus.getAbsolutePath() + " is not a directory", rootCorpus.isDirectory());
+
+		// start: creating and setting corpus definition
+		CorpusDesc corpDef = new CorpusDesc();
+		FormatDesc formatDef = new FormatDesc();
+		formatDef.setFormatName("xml");
+		formatDef.setFormatVersion("1.0");
+		corpDef.setFormatDesc(formatDef);
+		corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
+		this.getFixture().setCorpusDesc(corpDef);
+		// end: creating and setting corpus definition
+
+		// runs the PepperModule
 		this.start();
-		
-//		SCorpusGraph importedSCorpusGraph= getFixture().getSCorpusGraph();
+
+		// SCorpusGraph importedSCorpusGraph= getFixture().getSCorpusGraph();
 		assertNotNull(getFixture().getSCorpusGraph());
 		assertNotNull(getFixture().getSCorpusGraph().getSCorpora());
 		assertEquals(1, getFixture().getSCorpusGraph().getSCorpora().size());
 		assertNotNull(getFixture().getSCorpusGraph().getSCorpora().get(0));
 		assertNotNull(getFixture().getSCorpusGraph().getSDocuments());
 		assertNotNull(getFixture().getSCorpusGraph().getSDocuments().get(0));
-		
-		if (getFixture().getSCorpusGraph().getSDocuments().get(0).getSDocumentGraph()== null){
+
+		if (getFixture().getSCorpusGraph().getSDocuments().get(0).getSDocumentGraph() == null) {
 			getFixture().getSCorpusGraph().getSDocuments().get(0).loadSDocumentGraph();
 		}
-		
-		SDocumentGraph sDocGraph= getFixture().getSCorpusGraph().getSDocuments().get(0).getSDocumentGraph();
-		
-		System.out.println("docs: "+ getFixture().getSCorpusGraph().getSDocuments());
-		
+
+		SDocumentGraph sDocGraph = getFixture().getSCorpusGraph().getSDocuments().get(0).getSDocumentGraph();
+
 		assertNotNull(sDocGraph);
 		assertNotNull(sDocGraph.getSTextualDSs());
 		assertEquals(1, sDocGraph.getSTextualDSs().size());
 		assertNotNull(sDocGraph.getSTokens());
 		assertEquals(3, sDocGraph.getSTokens().size());
 		assertNotNull(sDocGraph.getSTokens().get(0));
-		
+
 		assertNotNull(sDocGraph.getSTokens().get(0).getSAnnotations());
 		assertEquals(2, sDocGraph.getSTokens().get(0).getSAnnotations().size());
 		assertEquals("PPER", sDocGraph.getSTokens().get(0).getSAnnotation("mycorpus::pos").getSValue());
 		assertEquals(2, sDocGraph.getSTokens().get(0).getSAnnotations().size());
 		assertEquals("I", sDocGraph.getSTokens().get(0).getSAnnotation("mycorpus::lemma").getSValue());
-		
+
 		assertNotNull(sDocGraph.getSTokens().get(1));
 		assertEquals(2, sDocGraph.getSTokens().get(1).getSAnnotations().size());
 		assertEquals("VBP", sDocGraph.getSTokens().get(1).getSAnnotation("mycorpus::pos").getSValue());
 		assertEquals(2, sDocGraph.getSTokens().get(1).getSAnnotations().size());
 		assertEquals("have", sDocGraph.getSTokens().get(1).getSAnnotation("mycorpus::lemma").getSValue());
 	}
-	
+
 	@Test
-	public void testMultiFeat_MetaData()
-	{
-		File rootCorpus= new File(getTestResources()+"multiFeat_MetaData/"+"myCorpus/");
-		
-		//start: creating and setting corpus definition
-			CorpusDesc corpDef= new CorpusDesc();
-			FormatDesc formatDef= new FormatDesc();
-			formatDef.setFormatName("xml");
-			formatDef.setFormatVersion("1.0");
-			corpDef.setFormatDesc(formatDef);
-			corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
-			this.getFixture().setCorpusDesc(corpDef);
-		//end: creating and setting corpus definition
-		
-		//runs the PepperModule
+	public void testMultiFeat_MetaData() {
+		File rootCorpus = new File(getTestResources() + "multiFeat_MetaData/" + "myCorpus/");
+
+		// start: creating and setting corpus definition
+		CorpusDesc corpDef = new CorpusDesc();
+		FormatDesc formatDef = new FormatDesc();
+		formatDef.setFormatName("xml");
+		formatDef.setFormatVersion("1.0");
+		corpDef.setFormatDesc(formatDef);
+		corpDef.setCorpusPath(URI.createFileURI(rootCorpus.getAbsolutePath()));
+		this.getFixture().setCorpusDesc(corpDef);
+		// end: creating and setting corpus definition
+
+		// runs the PepperModule
 		this.start();
-		
-		SCorpusGraph importedSCorpusGraph= getFixture().getSaltProject().getSCorpusGraphs().get(0);
+
+		SCorpusGraph importedSCorpusGraph = getFixture().getSaltProject().getSCorpusGraphs().get(0);
 		assertNotNull(importedSCorpusGraph.getSCorpora());
-		assertEquals(1,importedSCorpusGraph.getSCorpora().size());
+		assertEquals(1, importedSCorpusGraph.getSCorpora().size());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0));
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations());
-		assertEquals(2,importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations().size());
+		assertEquals(2, importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotations().size());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author"));
-		assertEquals("John Doe",importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author").getSValue());
+		assertEquals("John Doe", importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("author").getSValue());
 		assertNotNull(importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang"));
-		assertEquals("eng",importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang").getSValue());
-		
-		assertEquals(1,importedSCorpusGraph.getSDocuments().size());
+		assertEquals("eng", importedSCorpusGraph.getSCorpora().get(0).getSMetaAnnotation("lang").getSValue());
+
+		assertEquals(1, importedSCorpusGraph.getSDocuments().size());
 		assertNotNull(importedSCorpusGraph.getSDocuments().get(0));
 		assertNotNull(importedSCorpusGraph.getSDocuments().get(0).getSMetaAnnotations());
 		assertNotNull(importedSCorpusGraph.getSDocuments().get(0).getSMetaAnnotation("date"));
-		assertEquals("today",importedSCorpusGraph.getSDocuments().get(0).getSMetaAnnotation("date").getSValue());
+		assertEquals("today", importedSCorpusGraph.getSDocuments().get(0).getSMetaAnnotation("date").getSValue());
 	}
 }
