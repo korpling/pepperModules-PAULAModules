2ba195892c3d4e498de710718ac06ef6d33850d9
florian <f.zipser@gmx.de>
2016-04-28 21:51:56 +0200

    started implementing isImportable

--- src/main/java/org/corpus_tools/peppermodules/paula/PAULAImporter.java
+++ src/main/java/org/corpus_tools/peppermodules/paula/PAULAImporter.java
@@ -17,8 +17,16 @@
  */
 package org.corpus_tools.peppermodules.paula;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
+import org.apache.commons.io.FileUtils;
 import org.corpus_tools.pepper.impl.PepperImporterImpl;
 import org.corpus_tools.pepper.modules.PepperImporter;
 import org.corpus_tools.pepper.modules.PepperMapper;
@@ -56,9 +64,67 @@ public class PAULAImporter extends PepperImporterImpl implements PepperImporter 
 	 */
 	private String[] PAULA_FILE_ENDINGS = { "xml", "paula" };
 
+	private Collection<File> sampleFiles(File dir, int numberOfSampledFiles, String... fileEndings) {
+
+		Collection<File> files = FileUtils.listFiles(dir, fileEndings, true);
+		File[] allFiles = new File[files.size()];
+		allFiles = files.toArray(allFiles);
+		Collection<File> sampledFiles = new ArrayList<>(numberOfSampledFiles);
+		Random randomGenerator = new Random();
+		int maxFiles= (numberOfSampledFiles > allFiles.length)? allFiles.length: numberOfSampledFiles; 
+		for (int i = 0; i < maxFiles; i++) {
+			sampledFiles.add(allFiles[randomGenerator.nextInt(allFiles.length)]);
+		}
+		return sampledFiles;
+	}
+
+	private String readLines(File file, int numOfLinesToRead) {
+		StringBuilder fileContent = new StringBuilder();
+		try (LineNumberReader reader = new LineNumberReader(new InputStreamReader(new FileInputStream(file), "UTF-8"))) {
+			String line;
+			while (((line = reader.readLine()) != null) && reader.getLineNumber() <= numOfLinesToRead) {
+				fileContent.append(line);
+				fileContent.append(System.lineSeparator());
+			}
+		} catch (IOException e) {
+
+		}
+
+		return fileContent.toString();
+	}
+
+	private Collection<String> readFileContents(URI corpusPath, int numberOfSampledFiles, int numberOfLines, String... fileEndings){
+		File dir = new File(corpusPath.toFileString());
+		Collection<File> sampledFiles = sampleFiles(dir, numberOfSampledFiles);
+		Collection<String> fileContents= new ArrayList<>(sampledFiles.size());
+		for (File sampleFile: sampledFiles){
+			fileContents.add(readLines(sampleFile, numberOfLines));
+		}
+		return fileContents;
+	}
+	
+	@Override
+	public Double isImportable(URI corpusPath) {
+		Double retValue= 0.0;
+		
+		if (corpusPath == null) {
+			return retValue;
+		}
+		int numberOfSampledFiles = 10;
+		int numberOfLines = 10;
+		
+		for (String content: readFileContents(corpusPath, numberOfSampledFiles, numberOfLines, PAULA_FILE_ENDINGS)){
+			if ((content.contains("<?xml version=\"1.0\"")) && (content.contains("<paula version="))){
+				retValue= 1.0;
+				break;
+			}
+		}
+		return retValue;
+	}
+
 	/**
-	 * Creates a mapper of type {@link PAULA2SaltMapper}. {@inheritDoc
-	 * PepperModule#createPepperMapper(Identifier)}
+	 * Creates a mapper of type {@link PAULA2SaltMapper}.
+	 * {@inheritDoc PepperModule#createPepperMapper(Identifier)}
 	 */
 	@Override
 	public PepperMapper createPepperMapper(Identifier Identifier) {
