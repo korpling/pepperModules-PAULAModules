605e56b17546348fb9cfeacaa18e9f002eef6d83
florian <f.zipser@gmx.de>
2016-09-04 16:11:14 +0200

    fixed PAULAExporter self-test

--- src/test/java/org/corpus_tools/peppermodules/paula/PAULAExporterTest.java
+++ src/test/java/org/corpus_tools/peppermodules/paula/PAULAExporterTest.java
@@ -19,7 +19,13 @@ package org.corpus_tools.peppermodules.paula;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.corpus_tools.pepper.common.CorpusDesc;
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.corpus_tools.pepper.common.FormatDesc;
 import org.corpus_tools.pepper.common.ModuleFitness;
 import org.corpus_tools.pepper.common.ModuleFitness.FitnessFeature;
@@ -28,29 +34,24 @@ import org.corpus_tools.pepper.testFramework.PepperExporterTest;
 import org.corpus_tools.pepper.testFramework.PepperTestUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.corpus_tools.salt.util.*;
+import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.eclipse.emf.common.util.URI;
 
 public class PAULAExporterTest extends PepperExporterTest {
-	private PAULAExporter fixture = null;
 
 	@Before
 	public void beforeEach() {
-		fixture = new PAULAExporter();
-
-		// set formats to support
-		FormatDesc formatDef = new FormatDesc();
-		formatDef.setFormatName(PAULAExporter.FORMAT_NAME);
-		formatDef.setFormatVersion(PAULAExporter.FORMAT_VERSION);
-		this.addSupportedFormat(formatDef);
-
-		// set corpus definition
-		CorpusDesc corpDef = new CorpusDesc();
-		corpDef.setFormatDesc(formatDef);
+		super.setFixture(new PAULAExporter());
+		this.addSupportedFormat(new FormatDesc.FormatDescBuilder().withName(PAULAExporter.FORMAT_NAME)
+				.withVersion(PAULAExporter.FORMAT_VERSION).build());
 	}
 
 	@Test
 	public void whenSelfTestingModule_thenResultShouldBeTrue() {
-
-		final ModuleFitness fitness = new ModuleFitnessChecker(PepperTestUtil.createDefaultPepper()).selfTest(fixture);
+		final ModuleFitness fitness = new ModuleFitnessChecker(PepperTestUtil.createDefaultPepper()).selfTest(getFixture());
 		assertThat(fitness.getFitness(FitnessFeature.HAS_SELFTEST)).isTrue();
 		assertThat(fitness.getFitness(FitnessFeature.HAS_PASSED_SELFTEST)).isTrue();
 	}
